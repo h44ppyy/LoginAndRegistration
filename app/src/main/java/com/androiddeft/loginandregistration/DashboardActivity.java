65fc60fb763322e6d863209fac8629095795708d
happy <happynekocan@gmail.com>
2018-12-24 09:34:40 +0700

    penambahan fragment dan bottom nav

--- app/src/main/java/com/androiddeft/loginandregistration/DashboardActivity.java
+++ app/src/main/java/com/androiddeft/loginandregistration/DashboardActivity.java
@@ -1,22 +1,33 @@
 package com.androiddeft.loginandregistration;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class DashboardActivity extends AppCompatActivity {
-    private SessionHandler session;
+public class DashboardActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {    private SessionHandler session;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         session = new SessionHandler(getApplicationContext());
         User user = session.getUserDetails();
         TextView welcomeText = findViewById(R.id.welcomeText);
+
+        // kita set default nya Home Fragment
+        loadFragment(new HomeFragment());
+        // inisialisasi BottomNavigaionView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         welcomeText.setText("Welcome "+user.getFullName()+", your session will expire on "+user.getSessionExpiryDate());
 
@@ -32,5 +43,36 @@ public class DashboardActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.search_menu:
+                fragment = new SearchFragment();
+                break;
+            case R.id.favorite_menu:
+                fragment = new FavoriteFragment();
+                break;
+            case R.id.account_menu:
+                fragment = new AccountFragment();
+                break;
+
+        }
+        return loadFragment(fragment);
     }
 }
